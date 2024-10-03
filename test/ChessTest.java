ae07d423793489285b938fc726dd68a0450955e9
olegmakeienko <molva83@gmail.com>
2024-10-03 13:58:28 +0200

    testWhiteKingPosition added

--- test/ChessTest.java
+++ test/ChessTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ChessTest {
@@ -10,5 +11,12 @@ public class ChessTest {
     }
 
     @Test
-    public void testGetTile
+    public void testWhiteKingPosition() {
+        ChessBoard board = new ChessBoard();
+        Piece piece = ChessInitialPosition.WHITE_KING.toPiece();
+        ChessInitialPosition position = ChessInitialPosition.WHITE_KING;
+        board.setTile(position.getX(), position.getY(), piece);
+
+        assertEquals(ChessInitialPosition.WHITE_KING.toPiece(), board.getTile(5, 1));
+    }
 }
